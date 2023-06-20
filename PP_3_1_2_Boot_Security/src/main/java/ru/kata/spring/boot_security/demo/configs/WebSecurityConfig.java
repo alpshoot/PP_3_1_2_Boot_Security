a0fbd042b217f9433b98b50ebbe0cf97340c96bc
Alekcandr Dmitriev <126895869+alpshoot@users.noreply.github.com>
2023-06-21 00:13:18 +0300

    Add files via upload

--- PP_3_1_2_Boot_Security/src/main/java/ru/kata/spring/boot_security/demo/configs/WebSecurityConfig.java
+++ PP_3_1_2_Boot_Security/src/main/java/ru/kata/spring/boot_security/demo/configs/WebSecurityConfig.java
@@ -2,6 +2,8 @@ package ru.kata.spring.boot_security.demo.configs;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -40,6 +42,16 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(HttpSecurity http, PasswordEncoder passwordEncoder
+            , UserDetailsService userDetailService) throws Exception {
+        return http.getSharedObject(AuthenticationManagerBuilder.class)
+                .userDetailsService(userDetailService)
+                .passwordEncoder(passwordEncoder)
+                .and()
+                .build();
     }
 
     // аутентификация inMemory
